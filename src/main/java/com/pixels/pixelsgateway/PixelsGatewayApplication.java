3a2c803696d2ea268acc407b9c4cb867b6898cb6
a321bhi <a321bhi@gmail.com>
2022-08-05 13:04:08 +0530

    update

--- src/main/java/com/pixels/pixelsgateway/PixelsGatewayApplication.java
+++ src/main/java/com/pixels/pixelsgateway/PixelsGatewayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PixelsGatewayApplication {
 	@Value("${application.microservice.user}")
 	private String userServiceUrl; 
